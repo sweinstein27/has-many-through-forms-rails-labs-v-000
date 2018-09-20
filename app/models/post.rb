a4201e4286aed39e0fe184519a766e84fdcdbc25
Scott Weinstein <sweinstein27@gmail.com>
2018-09-20 16:28:30 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ActiveRecord::Base
   def categories_attributes=(categories_hashes)
     categories_hashes.each do |i, categories_attributes|
       category = Category.find_or_create_by(name: categories_attributes[:name])
+      self.post_categories.build(:category => category)
   end
 
 
