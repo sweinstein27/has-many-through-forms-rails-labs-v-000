2447e3b70c77487c20552138fc660cd72d3ae962
Scott Weinstein <sweinstein27@gmail.com>
2018-09-20 16:24:02 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(categories_attributes)
-
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, categories_attributes|
+      category = Category.find_or_create_by()
   end
 
 
