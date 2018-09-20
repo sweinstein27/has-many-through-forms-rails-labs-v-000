90afcec8ae43bd1c9efef08ad2f86e5aebbbcbad
Scott Weinstein <sweinstein27@gmail.com>
2018-09-20 15:56:58 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,11 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
   end
 
   def create
-    post = Post.create(post_params)
+    @post = Post.create(post_params)
     redirect_to post
   end
 
